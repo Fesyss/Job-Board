46e25a7a40ba00b3713cb6db55826f099be85734
Fesyss <69641743+Fesyss@users.noreply.github.com>
2024-05-24 06:42:02 +0200

    rework of the project

--- js/main.js
+++ js/main.js
@@ -1,21 +1,35 @@
+
     function openLink() {
-        var job = document.getElementById('jobInput').value;
-        var location = document.getElementById('locationInput').value;
+        var job = document.getElementById('jobInput').value.trim();
+        var location = document.getElementById('locationInput').value.trim();
         
-        if (job === "Sales manager" && location === "Poznan") {
-            window.open('/html/salesManagerPoznan.html', '_blank');
-        }
-        if (job === "Sales manager" && location === "") {
-            window.open('/html/salesManager.html', '_blank');
-        }
-        if (job === "Sales manager" && location === "remote") {
-            window.open('/html/salesManagerRemote.html', '_blank');
-        }
-        if (job === "Project manager" && location === "") {
-            window.open('/html/projectManager.html', '_blank');
-        }
-        if (job === "Project manager" && location === "remote") {
-            window.open('/html/projectManagerRemote.html', '_blank');
-        }
+        // Combine job and location into a single string
+        var jobLocation = job + "|" + location;
         
+        switch (jobLocation) {
+            case "Sales manager|Warsaw":
+                window.open('/html/salesManager/salesManagerWarsaw.html', '_blank');
+                break;
+            case "Sales manager|Poznan":
+                window.open('/html/salesManager/salesManagerPoznan.html', '_blank');
+                break;
+            case "Sales manager|":
+                window.open('/html/salesManager/salesManager.html', '_blank');
+                break;
+            case "Sales manager|remote":
+                window.open('/html/salesManager/salesManagerRemote.html', '_blank');
+                break;
+            case "Project manager|":
+                window.open('/html/projectManager/projectManager.html', '_blank');
+                break;
+                case "Project manager|Chelm":
+                window.open('/html/projectManager/projectManagerChelm.html', '_blank');
+                break;
+            case "Project manager|remote":
+                window.open('/html/projectManager/projectManagerRemote.html', '_blank');
+                break;
+            default:
+                alert('No matching job listings found. Please check your input.');
+        }
     }
+    
